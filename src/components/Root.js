5445981683de357b54b7372a7d85aa8b989122bf
Natalie Estrada <nmestrad@gmail.com>
2020-04-01 11:54:36 -0500

    added create-react-app files

---
+++ src/components/Root.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Root = () => {
+    return (
+        <div>
+            <h1>Welcome to The Recipe Scaler!</h1>
+            <h3>Enter your recipe below</h3>
+        </div>
+    )
+}
+
+export default Root
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { Provider } from 'react-redux'
+// import {Router} from 'react-router-dom'
+// import history from './history'
+// import store from './redux/store'
+import Root from './components/Root'
+
+ReactDOM.render(
+     <Root/>,
+    document.getElementById('app')
+  )
